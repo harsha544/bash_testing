abca56f9f4e45d49ef2428b86d2bccd146785cc8
Krishna Harsha Voora <krishvoor@in.ibm.com>
2018-05-08 10:30:37 +0530

    Updating 	generate_crypto_functions.sh Adding 	test.sh, before generate_crypto_functions.sh is updated
    Signed-off-by: Krishna Harsha Voora <krishvoor@in.ibm.com>

--- generate_crypto_functions.sh
+++ generate_crypto_functions.sh
@@ -5,8 +5,26 @@ NOP=$1
 
 if [ $NOP -gt 2 ]
 then
-  echo "Updating Crypto Material" $NOP
+  echo "Updating Crypto Material"
+  # pushd ../sampleconfig/
+  cp crypto-config.yaml.orig crypto-config.yaml
+  for ((i=3; i<=$NOP; i++))
+  do
+	echo "  # ---------------------------------------------------------------------------" >> crypto-config.yaml
+	echo "  # Org$i: See "Org$i" for full specification" >> crypto-config.yaml
+	echo "  # ---------------------------------------------------------------------------" >> crypto-config.yaml	
+	echo "  - Name: Org$i" >> crypto-config.yaml
+	echo "    Domain: org$i.example.com" >> crypto-config.yaml
+	echo "    Template:" >> crypto-config.yaml
+	echo "      Count: 2" >> crypto-config.yaml
+	echo "    Users:" >> crypto-config.yaml
+	echo "      Count: 1"   >> crypto-config.yaml
+  done 
+  # popd
 else
   NOP=2
+  # pushd ../sampleconfig/
+  cp crypto-config.yaml.orig crypto-config.yaml 
   echo "Nothing to be done" $NOP
+  # popd
 fi
---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+i=$1
+echo "  # ---------------------------------------------------------------------------"
+echo "  # Org$i: See "Org1" for full specification"
+echo "  # ---------------------------------------------------------------------------"
+echo "  - Name: Org$i"
+echo "    Domain: org$i.example.com"
+echo "    Template:"
+echo "      Count: 2"
+echo "    Users:"
+echo "      Count: 1"  
